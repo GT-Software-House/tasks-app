7b0c9b6ce6ee9de1a4134196cce12d6b775065d4
Gabriel Santana <chaves123.gs@gmail.com>
2025-03-09 10:52:39 -0300

    modularization: rename folders

--- feature/preferences/src/androidDeviceTest/kotlin/org/gabrielsantana/quicknotes/feature/preferences/ExampleInstrumentedTest.kt
+++ feature/preferences/src/androidDeviceTest/kotlin/org/gabrielsantana/quicknotes/feature/preferences/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package org.gabrielsantana.quicknotes.preferences
+package org.gabrielsantana.quicknotes.feature.preferences
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
