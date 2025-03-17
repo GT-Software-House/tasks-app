30ebbde6936db6936b9dffab474fd049bb64c3db
Gabriel Santana <chaves123.gs@gmail.com>
2025-03-17 15:26:05 -0300

    move out from android library multiplatform plugin

--- feature/home/src/androidInstrumentedTest/kotlin/org/gabrielsantana/quicknotes/feature/home/ExampleInstrumentedTest.kt
+++ feature/home/src/androidInstrumentedTest/kotlin/org/gabrielsantana/quicknotes/feature/home/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.gabrielsantana.tasks.home.test", appContext.packageName)
+        assertEquals("org.gabrielsantana.quicknotes.feature.home.test", appContext.packageName)
     }
 }
